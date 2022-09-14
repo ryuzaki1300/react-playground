04dd0c56f0c550753602f69a13acccad182a17a1
ryuzaki1300l@gmail.com <ryuzaki1300l@gmail.com>
2022-09-14 14:15:54 +0430

    setup packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TestComponent from './Components/TestComponent';
 
 function App() {
   return (
+  <Router>
     <div className="App">
       <header className="App-header">
-        <div className="test-class">Test</div>
+      <Switch>
+          <Route exact path="/">
+            <TestComponent />
+          </Route>
+        </Switch>
       </header>
     </div>
+  </Router>
   );
 }
 
---
+++ src/Components/TestComponent.js
@@ -0,0 +1,9 @@
+const TestComponent = () => {
+    return ( 
+        <div className="test-component container border p-5">
+            <h3 className="text-center">Test Component</h3>
+        </div>
+     );
+}
+ 
+export default TestComponent;
